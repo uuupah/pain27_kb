afed90b74d181582013526e7202a20336f898e9d
Pepega1568 <74371168+Pepega1568@users.noreply.github.com>
2022-04-29 22:46:20 +0300

    Add files via upload

---
+++ QMK_VIAL/keymaps/keymap.c
@@ -0,0 +1,26 @@
+#include "pain27.h"
+
+enum layer_names {
+    _BASE,
+    _FN,
+    _ADJ
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+	[_BASE] = LAYOUT(
+		KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_P, 
+		MT(MOD_LCTL, KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_O, 
+		MT(MOD_LSFT, KC_Z), KC_X, KC_C, KC_V, LT(1, KC_SPC), KC_B, KC_N, MT(MOD_LSFT, KC_M), LT(2, KC_L)),
+
+	[_ADJ] = LAYOUT(
+		KC_ESC, KC_1, KC_2, KC_3, KC_EQL, KC_TRNS, KC_TRNS, KC_UP, KC_BSPC, 
+		KC_TRNS, KC_4, KC_5, KC_6, KC_MINS, KC_SLSH, KC_LEFT, KC_RGHT, KC_TRNS, 
+		MT(MOD_LSFT, KC_0), KC_7, KC_8, KC_9, KC_TRNS, KC_PLUS, KC_ASTR, KC_DOWN, KC_ENT),
+
+	[_FN] = LAYOUT(
+		KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_TRNS, KC_PGUP, KC_DEL, 
+		KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_HOME, KC_END, KC_TRNS, 
+		KC_DOT, KC_COMM, KC_QUOT, KC_DQUO, KC_TRNS, KC_LPRN, KC_RPRN, KC_PGDN, KC_TRNS)
+};
+
--- QMK_VIAL/keymaps/rules.mk
+++ QMK_VIAL/keymaps/rules.mk
@@ -1,1 +1,7 @@
-
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIAL_INSECURE = yes
+LTO_ENABLE = yes
+QMK_SETTINGS = no
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes 
