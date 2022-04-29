049b1462a330cfc7d9ffd1696f3f99c2581876c8
Pepega1568 <74371168+Pepega1568@users.noreply.github.com>
2022-04-29 22:32:25 +0300

    Add files via upload

---
+++ QMK_VIA/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes     # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes            # USB Nkey Rollover
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes 
+VIA_ENABLE = yes
+
